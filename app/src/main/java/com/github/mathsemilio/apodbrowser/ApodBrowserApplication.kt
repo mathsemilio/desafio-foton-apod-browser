1972f6c3fcd4bba68147813b21c76a4eb4acf6f2
Matheus Menezes <mathsemilio@gmail.com>
2021-11-17 21:09:23 -0300

    Configuração inicial do projeto Declaração das depenpências iniciais Remoção de arquivos desnecessários Criação da classe de aplicação customizada Criação de uma estrutura inicial de pacotes Declaração de persmissões necessárias Adição de regra para a exclusão do arquivo .idea/misc.xml

---
+++ app/src/main/java/com/github/mathsemilio/apodbrowser/ApodBrowserApplication.kt
@@ -0,0 +1,10 @@
+package com.github.mathsemilio.apodbrowser
+
+import android.app.Application
+
+class ApodBrowserApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/github/mathsemilio/apodbrowser/features/apodlist/ApodListActivity.kt
@@ -0,0 +1,13 @@
+package com.github.mathsemilio.apodbrowser.features.apodlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.mathsemilio.apodbrowser.R
+
+class ApodListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apod_list)
+    }
+}
